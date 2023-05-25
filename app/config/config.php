e4fc1d1aa293c2805b1673a7f66e34228cf4a73b
Tjardo Ihrig <tjardoihrig.zakelijk@gmail.com>
2023-05-25 13:51:41 +0200

    Config URLROOT

---
+++ app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define("URLROOT", "http://mvc-oop-pdo-proeftoets-userstory.com");
+define("APPROOT", dirname(dirname(__FILE__)));
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'instructeursindienst');
