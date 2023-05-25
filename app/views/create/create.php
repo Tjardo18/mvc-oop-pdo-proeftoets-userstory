a7259d9ff3f5f2519c97556535facd1b652870d9
Tjardo Ihrig <tjardoihrig.zakelijk@gmail.com>
2023-05-25 13:52:58 +0200

    Views

---
+++ app/views/create/create.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= URLROOT; ?>/css/style.css">
+    <title>Create</title>
+</head>
+
+<body>
+
+    <header>
+        <a href="<?= URLROOT; ?>" class="logo">
+            <i class="ri-home-3-fill"></i>
+            <span>TJARDO</span>
+        </a>
+
+        <ul class="navbar">
+            <li><a href="<?= URLROOT; ?>">Home</a></li>
+            <li><a href="<?= URLROOT; ?>/instructeur" class="active">Instructeurs</a></li>
+        </ul>
+
+        <div class="main">
+            <a href="#" class="user">
+                <i class="ri-user-fill"></i>
+                Sign In
+            </a>
+            <a href="#">Register</a>
+            <div class="bx bx-menu" id="menu-icon"></div>
+        </div>
+    </header>
+
+    <div class="container">
+        <div class="circle"></div>
+        <div class="circle"></div>
+
+        <div class="card">
+            <div class="ruimte">
+                <h2><?= $data['title']; ?></h2>
+            </div>
+        </div>
+    </div>
+
+    <script src="<?= URLROOT; ?>/js/nav.js"></script>
+
+</body>
+
+</html>
